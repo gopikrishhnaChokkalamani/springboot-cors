6736e1573b9203c1473b1813af383c1cf8ba5737
Gopikrishhna <gopikrishhna@gmail.com>
2018-02-25 20:19:30 -0500

    testing

--- springboot-cors/src/main/java/com/gi/springboot/AppConfig.java
+++ springboot-cors/src/main/java/com/gi/springboot/AppConfig.java
@@ -1,13 +1,12 @@
 package com.gi.springboot;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class AppConfig extends WebMvcConfigurerAdapter {
 
-	// Comment the @CrossOrigin at RestController and enable this. This is Cors enablement through java config
+	// Comment the @CrossOrigin at RestController and enable this. This is CORS Enablement through java config
 	/*@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("Content-Type")
--- springboot-cors/src/main/java/com/gi/springboot/TestController.java
+++ springboot-cors/src/main/java/com/gi/springboot/TestController.java
@@ -1,17 +1,17 @@
 package com.gi.springboot;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = { "*" }, exposedHeaders = {
-		"hi", "Gopi" }, maxAge = 6000)
+		"hi", "User" }, maxAge = 6000)
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public String getMethod() {
 		return "hello";
 	}
